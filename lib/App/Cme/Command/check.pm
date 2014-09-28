c0cb358e5270bd1f674ed09e78057fc961535614
Dominique Dumont <dod@debian.org>
2014-09-28 20:46:16 +0200

    added inline help to check command

--- lib/App/Cme/Command/check.pm
+++ lib/App/Cme/Command/check.pm
@@ -1,3 +1,5 @@
+# ABSTRACT: Check the configuration of an application
+
 package App::Cme::Command::check ;
 use strict;
 use warnings;
@@ -9,8 +11,26 @@ use base qw/App::Cme::Common/;
 
 use Config::Model::ObjTreeScanner;
 
-sub opt_spec {
-  return ();
+sub usage_desc {
+  my ($self) = @_;
+  my $desc = $self->SUPER::usage_desc; # "%c COMMAND %o"
+  return "$desc [application]";
+}
+
+sub description {
+    return << "EOD"
+Checks the content of the configuration file of an application. Prints warnings and errors on STDOUT.
+
+Example:
+
+   cme check fstab
+
+Some applications will allow to override the default configuration file. For instance:
+
+curl http://metadata.ftp-master.debian.org/changelogs/main/f/frozen-bubble/unstable_copyright \
+| cme check dpkg-copyright -";
+EOD
+
 }
 
 sub execute {
@@ -27,3 +47,4 @@ sub execute {
 }
 
 1;
+
