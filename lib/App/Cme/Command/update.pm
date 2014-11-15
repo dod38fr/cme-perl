875a9bdd93695d787b4e00ef366b44620b8a9e00
Dominique Dumont <dod@debian.org>
2014-11-15 18:49:56 +0100

    cme: added update command

---
+++ lib/App/Cme/Command/update.pm
@@ -0,0 +1,71 @@
+# ABSTRACT: Update the configuration of an application
+
+package App::Cme::Command::update ;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+use App::Cme -command ;
+
+use base qw/App::Cme::Common/;
+
+use Config::Model::ObjTreeScanner;
+
+sub validate_args {
+    my ($self, $opt, $args) = @_;
+    $self->process_args($opt,$args);
+}
+
+sub opt_spec {
+    my ( $class, $app ) = @_;
+    return ( 
+        [ "save!"     => "Force a save even if no change was done" ],
+        [ "backup:s"  => "Create a backup of configuration files before saving." ],
+        $class->global_options,
+    );
+}
+
+sub usage_desc {
+  my ($self) = @_;
+  my $desc = $self->SUPER::usage_desc; # "%c COMMAND %o"
+  return "$desc [application] [file | ~~ ]";
+}
+
+sub description {
+    return << "EOD"
+Update a configuration file. The update is done scanning external ressource. For instance,
+the update of dpkg-copyright is done by scanning the headers of source files. (Actually, only
+dpkg-copyright model currently supports updates)
+command)
+
+Example:
+
+   cme update dpkg-copyright
+
+EOD
+
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+
+    my ($model, $inst, $root) = $self->init_cme($opt,$args);
+
+    say "update data" unless $opt->{quiet};
+    my $hook = sub {
+        my ($scanner, $data_ref,$node,@element_list) = @_;
+        $node->update() if $node->can('update') ;
+    }
+    Config::Model::ObjTreeScanner->new(
+        node_content_hook => $hook,
+        leaf_cb => sub { }
+    )->scan_node( undef, $root );
+
+    say "update done" unless $opt->{quiet};
+
+    $self->save($inst,$opt) ;
+}
+
+1;
+
