d6f63de377844fb737af1576fa9e3c13a723171f
Dominique Dumont <dod@debian.org>
2015-11-06 21:27:44 +0100

    cme: fixed -try-app-as-model option

--- lib/App/Cme/Common.pm
+++ lib/App/Cme/Common.pm
@@ -44,7 +44,7 @@ sub process_args {
     my $application = shift @$args;
 
     my $root_model = $appli_map->{$application};
-    $root_model ||= $application if $opt->{try_application_as_model};
+    $root_model ||= $application if $opt->{try_app_as_model};
     say "cme: using $root_model model" unless $opt->{quiet};
 
     Config::Model::Exception::Any->Trace(1) if $opt->{trace};
