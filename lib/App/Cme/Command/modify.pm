c9d04f4ded7b4fc1c34a8ed4855d71c9298b2468
Dominique Dumont <dod@debian.org>
2026-03-19 19:15:13 +0100

    modify: add "--commit <msg>" option

--- lib/App/Cme/Command/modify.pm
+++ lib/App/Cme/Command/modify.pm
@@ -4,7 +4,9 @@ package App::Cme::Command::modify ;
 
 use strict;
 use warnings;
-use 5.10.1;
+use v5.20;
+use feature qw/postderef signatures/;
+no warnings qw/experimental::postderef experimental::signatures/;
 
 use App::Cme -command ;
 
@@ -26,6 +28,7 @@ sub opt_spec {
     my ( $class, $app ) = @_;
     return ( 
         [ "backup:s"  => "Create a backup of configuration files before saving." ],
+        [ "commit|c:s" => "commit change with message passed as argument" ],
         $class->cme_global_options,
     );
 }
@@ -41,21 +44,49 @@ sub description {
     return $self->get_documentation;
 }
 
+sub commit ($self, $msg) {
+    system(qw/git commit -a -m/, $msg) == 0
+        or die "git commit failed: $?\n";
+    return;
+}
+
 sub execute {
     my ($self, $opt, $args) = @_;
 
     $opt->{_verbose} = 'Loader' if $opt->{verbose};
 
+    my $stashed;
+
+    # stash pending work
+    if ($opt->{commit}) {
+        $stashed = $self->autostash;
+    }
+
     my ($model, $inst, $root) = $self->init_cme($opt,$args);
 
     # needed to create write_back subs
-    $root->dump_tree() if $opt->{save} and not @$args;
+    if ($opt->{save} and not @$args) {
+        $root->dump_tree();
+    }
 
     $root->load("@$args");
 
     $root->deep_check; # consistency check
 
-    $self->save($inst,$opt) ;
+    if ($inst->needs_save or $opt->{save}) {
+        $self->save($inst,$opt) ;
+
+        if ($opt->{commit}) {
+            $self->commit($opt->{commit});
+        }
+    }
+    elsif (not $opt->{quiet}) {
+        say "No change were applied";
+    }
+
+    if ($stashed) {
+        $self->pop_stash;
+    }
 
     return;
 }
@@ -118,6 +149,17 @@ modify xxx -save> is equivalent to running C<cme migrate xxx>.
 =item -verbose
 
 Show effect of the modify instructions.
+
+=item -commit
+
+Specify that the change must be committed with the passed commit
+message. When this option is used, C<cme> stashes and restores all
+modifications if used on a non-clean workspace. This option works only
+with L<git>.
+
+Example:
+
+  cme modify -commit "set Section parameter" dpkg-control 'source Section=perl'
 
 =back
 
