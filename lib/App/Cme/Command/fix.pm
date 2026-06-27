76b18a50a50af56137465abe92804c735580b379
Dominique Dumont <dod@debian.org>
2026-06-27 18:41:28 +0200

    fix: put back -backup option
    This was removed by mistake from cme v1.043
    
    This partly reverts commit cd7f6a023865a5b1a07132d6f56351f52863da64.

--- lib/App/Cme/Command/fix.pm
+++ lib/App/Cme/Command/fix.pm
@@ -21,6 +21,7 @@ sub opt_spec {
     my ( $class, $app ) = @_;
     return ( 
         [ "from=s@"  => "fix only a subset of a configuration tree" ],
+        [ "backup:s"  => "Create a backup of configuration files before saving." ],
         [ "filter=s" => "pattern to select the element name to be fixed"],
         $class->cme_global_options,
     );
